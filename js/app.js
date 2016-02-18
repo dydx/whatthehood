61ee8708e8b8b280016a7c170319087fabcd6b8e
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-02-17 19:02:08 -0500

    I think this thing works now

--- js/app.js
+++ js/app.js
@@ -1,28 +1,69 @@
 (function () {
-  angular.module('ghetto', []);
+  angular.module('ghetto', ['angularGeoFire']);
 
   angular.module('ghetto')
-    .factory('geolocation', function () {
-      // get geolocation in a closure and supply args in a callback
-      // which can be consumed by firebase / database layer later
-      return { 
-        location: ["-30.0000", "81.0000"]
-      }
-    });
+    .controller('ghettoCtrl', function ($scope, $log, $geofire) {
 
-  function show (data) {
-    return data;
-  }
+      var vm = this;
 
-  angular.module('ghetto')
-    .controller('ghettoCtrl', function ($log, geolocation) {
+      var firebase = new Firebase('https://whatthehood.firebaseio.com/');
+      var $geo = $geofire(firebase);
 
-      this.amI = function () {
-        $log.debug('Fetching information for', geolocation.location);
+      vm.results = [];
+
+      // The state of these things needs to he handled in a better way
+      // this is terrible
+      vm.inThaHood = false;
+      vm.hoodChecked = false;
+      vm.hoodNotified = false;
+      vm.locationLocked = false;
+
+      // debugging
+      $log.debug('Location Acquired: ', vm.locationLocked);
+      navigator.geolocation.getCurrentPosition(function (position) {
+        vm.coords = [
+          position.coords.latitude,
+          position.coords.longitude
+        ];
+        vm.locationLocked = true;
+        // debugging
+        $log.debug('Location Acquired: ', vm.locationLocked);
+      });
+
+      vm.amI = function () {
+        // this.inThaHood is determined by performing a geospacial query
+        vm.hoodChecked = vm.hoodChecked === true ? false : true;
+        vm.hoodNotified = false;
+
+        // GeoFire Query
+        var search = $geo.$query({
+          center: vm.coords,
+          radius: 1
+        });
+
+        var geoQueryCallback = search.on('key_entered', 'SEARCH:KEY_ENTERED');
+        $scope.$on("SEARCH:KEY_ENTERED", function (event, key, loc, distance) {
+          vm.results.push({key: key, loc: loc, distance: distance});
+
+          if (distance > 5) {
+            geoQueryCallback.cancel();
+          }
+        });
+
+        if (vm.results.length > 1) {
+          vm.inThaHood = true;
+        }
       };
 
-      this.iAm = function () {
-        $log.debug('Submitting information for', geolocation.location);
+      vm.iAm = function () {
+        vm.hoodChecked = false;
+        vm.hoodNotified = vm.hoodNotified === true ? false : true;
+
+        // GeoFire Insert
+        $geo.$set(Date.now().toString(), vm.coords)
+          .catch(function (err) {
+            $log.error(err);
+          });
       };
     });
 })();
